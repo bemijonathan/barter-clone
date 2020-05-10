2f75f3a2cfd95fbae32ac83a44ab36922be6f357
bemijonathan <bemijonathan@gmail.com>
2020-05-10 12:58:23 +0100

    added airtime component

--- src/components/Airtime.js
+++ src/components/Airtime.js
@@ -3,9 +3,14 @@ import React from 'react'
 export default function Airtime() {
     return (
         <div className="airtime fixed z-10 top-0 right-0 h-screen w-full bg-blue-600 bg-opacity-25 flex items-center justify-center">
-            <div className="tilt-in-fwd-tr p-3 w-1/3 shadow-lg bg-white">
-                <imput className="p-3 mb-5" type="number" />
-                <input className="p-3 mb-5" type="number" />
+            <div className="tilt-in-fwd-tr p-5 w-2/5 shadow-lg bg-white">
+                <div>
+                    <input className="p-3 mb-5 bg-blue-500 w-full" type="number" />
+                </div>
+                <div>
+                    <input className="p-3 mb-5 bg-blue-500 w-full" type="number" />
+                </div>
+
             </div>
         </div>
     )
