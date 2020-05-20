883ea84719b1a0460f9528b8d4fc14912d837ee7
bemijonathan <bemijonathan@gmail.com>
2020-05-20 20:37:26 +0100

    fixed authentication

--- src/App.js
+++ src/App.js
@@ -1,65 +1,32 @@
 import React from 'react'
 import { Router, Redirect } from '@reach/router'
-// import Home from './Pages/Home/Home';
-// import Nav from './components/Navbar'
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import DashboardHome from './Pages/DashboardHome';
 import Dashboard from './Pages/Dashboard';
 import Cards from './Pages/Cards'
 import Transactions from './Pages/Transactions';
-import { AirtimeContext , Authenticated} from './context/airtimeContext';
+import { AirtimeContext } from './context/airtimeContext';
 import 'react-notifications/lib/notifications.css';
-import jwt from 'jsonwebtoken'
+import {authentication} from './utils/auth';
 
 
 const App = () => {
   const [show, setShow] = React.useState(false)
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false)
-
   const AirtimeComponent = {
     show,
     hide: (value) => {
       setShow(value)
     }
   }
-  
-  const authentication = async () => {
-     const token = await localStorage.getItem('auth-token')
-    if(token){
-      const user = jwt.decode(token)
-      if(Math.floor(Date.now() / 1000) + (60 * 60) < user.exp){
-        setIsAuthenticated(true)
-      }else{
-        localStorage.removeItem("auth-token")
-        setIsAuthenticated(false)
-      }
-    }else{
-      setIsAuthenticated(false)
-    }    
-    console.log(isAuthenticated)
-  }
-
-  React.useEffect(() => {
-    authentication()
-  })
- 
 
   const ProtectedRoute = ({ component: Component, ...rest }) => {
-    console.log('*************', isAuthenticated)
-    return (
-    <Authenticated.Consumer>
-      {(isAuthenticated) =>
-        isAuthenticated ? <Component {...rest} /> : <Redirect from="" to="/" noThrow />
-      }
-    </Authenticated.Consumer>
-  )};
+    return ( authentication() ? <Component {...rest} /> : <Redirect from="" to="/" noThrow />)
+  }
 
   return (
-    <Authenticated.Provider value={isAuthenticated} >
     <AirtimeContext.Provider value={{ ...AirtimeComponent }}>
       <div>
-        {/* <Nav /> */}
         <Router>
           <ProtectedRoute path="dashboard" component={Dashboard} >
             <DashboardHome path="/" />
@@ -68,11 +35,9 @@ const App = () => {
           </ProtectedRoute>
           <Login path="/" />
           <Signup path="/signup" />
-
         </Router>
       </div>
     </AirtimeContext.Provider>
-    </Authenticated.Provider>
   )
 }
 
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,6 @@
 import axios from "../utils/axios"
+import jwt from 'jsonwebtoken'
+
     export async function newFunction(userDetails) {
         let { data } = await axios.post('auth/local/register', {
             username: `${userDetails.firstName} ${userDetails.lastName}`,
@@ -16,3 +18,19 @@ import axios from "../utils/axios"
         })
         return Errors
     }
+
+
+    export const authentication = async () => {
+        const token = await localStorage.getItem('auth-token')
+       if(token){
+         const user = jwt.decode(token)
+         if(Math.floor(Date.now() / 1000) + (60 * 60) < user.exp){
+           return true
+         }else{
+           localStorage.removeItem("auth-token")
+           return false
+         }
+       }else{
+         return false
+       }    
+     }
